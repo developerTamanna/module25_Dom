16ea8375b89dc0e907837591bc38aa291e413181
developerTamanna <astamanna57@gmail.com>
2025-02-22 20:52:00 +0600

    new section

---
+++ js/second.js
@@ -0,0 +1,11 @@
+console.log('second file')
+
+const secondList = document.getElementById('second-list')
+
+
+const li = document.createElement('li')
+li.innerText = 'my dynamic item'
+
+secondList.appendChild(li)
+
+
